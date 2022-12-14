d92786c799ebdf0cd33903c9d25a2aefe49490c5
hjrdave <dsanders_m633@outlook.com>
2022-12-14 10:19:20 -0500

    finished footer comp styles

--- src/widgets/organisms/Footer/Footer.tsx
+++ src/widgets/organisms/Footer/Footer.tsx
@@ -47,63 +47,71 @@ export default function Footer({ address, connect, markets, products, resources,
     return (
         <>
             <div className={`${styles.compContainer} text-white`}>
-                <Container className={'pt-4'}>
-                    <Row>
-                        <Col sm={12} md={2} className={'px-3'}>
-                            <Image path={(logoPath) ? logoPath : SCLogoWhite} className={'p-sm-4 p-md-0'}/>
-                            <p className={'pt-2 mb-0 text-muted text-sm-center text-md-start '}>{address?.street}</p>
-                            <p className={'mb-0 text-sm-center text-md-start text-muted'}>{address?.city}, {address?.state} {address?.zip}</p>
+                <Container>
+                    <Row className={'px-3'}>
+                        <Col sm={12} md={6} lg={2} className={'px-3 pt-4'}>
+                            <Image path={(logoPath) ? logoPath : SCLogoWhite} className={'p-sm-4 p-md-0'} />
+                            <p className={`pt-2 mb-0 text-sm-center text-md-start ${styles.address}`}>{address?.street}</p>
+                            <p className={`mb-0 text-sm-center text-md-start ${styles.address}`}>{address?.city}, {address?.state} {address?.zip}</p>
                         </Col>
-                        <Col className={'pt-sm-3 pt-md-0'}>
-                            <h6 className="text-uppercase fw-bold">Connect</h6>
-                            <div className={"pt-2"} style={{ width: '125px' }}>
-                                <div className='d-flex justify-content-around'>
-                                    <Link path={connect?.twitter} target={'_blank'}><i className={`fa-brands fa-twitter fs-4 ${styles.icon}`}></i></Link>
-                                    <Link path={connect?.facebook} target={'_blank'}><i className={`fa-brands fa-facebook fs-4 ${styles.icon}`}></i></Link>
-                                    <Link path={connect?.linkedin} target={'_blank'}><i className={`fa-brands fa-linkedin fs-4 ${styles.icon}`}></i></Link>
-                                </div>
-                                <div className='d-flex justify-content-around pt-3'>
-                                    <Link path={connect?.pinterest} target={'_blank'}><i className={`fa-brands fa-pinterest fs-4 ${styles.icon}`}></i></Link>
-                                    <Link path={connect?.youtube} target={'_blank'}><i className={`fa-brands fa-youtube fs-4 ${styles.icon}`}></i></Link>
-                                    <Link path={connect?.instagram} target={'_blank'}><i className={`fa-brands fa-instagram fs-4 ${styles.icon}`}></i></Link>
+                        <Col className={'d-flex justify-content-start justify-content-md-center pt-4'}>
+                            <div>
+                                <h6 className="text-uppercase fw-bold">Connect</h6>
+                                <div className={"pt-2"} style={{ width: '125px' }}>
+                                    <div className='d-flex justify-content-between'>
+                                        <Link path={connect?.twitter} target={'_blank'}><i className={`fa-brands fa-twitter fs-4 ${styles.icon}`}></i></Link>
+                                        <Link path={connect?.facebook} target={'_blank'}><i className={`fa-brands fa-facebook fs-4 ${styles.icon}`}></i></Link>
+                                        <Link path={connect?.linkedin} target={'_blank'}><i className={`fa-brands fa-linkedin fs-4 ${styles.icon}`}></i></Link>
+                                    </div>
+                                    <div className='d-flex justify-content-between pt-3'>
+                                        <Link path={connect?.pinterest} target={'_blank'}><i className={`fa-brands fa-pinterest fs-4 ${styles.icon}`}></i></Link>
+                                        <Link path={connect?.youtube} target={'_blank'}><i className={`fa-brands fa-youtube fs-4 ${styles.icon}`}></i></Link>
+                                        <Link path={connect?.instagram} target={'_blank'}><i className={`fa-brands fa-instagram fs-4 ${styles.icon}`}></i></Link>
+                                    </div>
                                 </div>
                             </div>
                         </Col>
-                        <Col className={'pt-sm-3 pt-md-0'}>
-                            <h6 className="text-uppercase fw-bold">Our Markets</h6>
-                            <ul className={`ps-0 ${styles.listGroup}`}>
-                                {
-                                    markets?.map((item) => (
-                                        <Link path={item.path} target={'_blank'}>
+                        <Col className={'d-flex justify-content-start justify-content-md-center pt-4'}>
+                            <div>
+                                <h6 className="text-uppercase fw-bold">Our Markets</h6>
+                                <ul className={`ps-0 ${styles.listGroup}`}>
+                                    {
+                                        markets?.map((item) => (
+                                            <Link path={item.path} target={'_blank'}>
+                                                <li>{item.text}</li>
+                                            </Link>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </Col>
+                        <Col sm={6} md={6} lg={3} className={'d-flex justify-content-start justify-content-md-center pt-4'}>
+                            <div>
+                                <h6 className="text-uppercase fw-bold">Products</h6>
+                                <ul className={`ps-0 ${styles.listGroup}`}>
+                                    {
+                                        products?.map((item) => (
+                                            <Link path={item.path} target={'_blank'}>
                                             <li>{item.text}</li>
                                         </Link>
-                                    ))
-                                }
-                            </ul>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
                         </Col>
-                        <Col sm={6} md={3} className={'mt-sm-3 mt-md-0'}>
-                            <h6 className="text-uppercase fw-bold">Products</h6>
-                            <ul className={`ps-0 ${styles.listGroup}`}>
-                                {
-                                    products?.map((item) => (
-                                        <Link path={item.path} target={'_blank'}>
-                                        <li>{item.text}</li>
-                                    </Link>
-                                    ))
-                                }
-                            </ul>
-                        </Col>
-                        <Col sm={6} md={3} className={'mt-sm-3 mt-md-0'}>
-                            <h6 className="text-uppercase fw-bold">Resources</h6>
-                            <ul className={`ps-0 ${styles.listGroup}`}>
-                                {
-                                    resources?.map((item) => (
-                                        <Link path={item.path} target={'_blank'}>
-                                        <li>{item.text}</li>
-                                    </Link>
-                                    ))
-                                }
-                            </ul>
+                        <Col sm={6} md={3} className={'d-flex justify-content-start justify-content-md-center pt-4'}>
+                            <div>
+                                <h6 className="text-uppercase fw-bold">Resources</h6>
+                                <ul className={`ps-0 ${styles.listGroup}`}>
+                                    {
+                                        resources?.map((item) => (
+                                            <Link path={item.path} target={'_blank'}>
+                                            <li>{item.text}</li>
+                                        </Link>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
